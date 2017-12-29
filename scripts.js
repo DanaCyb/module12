ac281c53023f6fb9b82dc7cf522ae03685fb2ee9
Danuta Cybulska <danuta.cybulska.biz@gmail.com>
2017-12-29 16:50:20 +0100

    Add code to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,21 @@
-var url = 'http://api.icndb.com/jokes/random';
-var button = document.getElementById('get-joke');
-button.addEventListener('click', function () {
-    getJoke();
-} );
-var paragraph = document.getElementById('joke');
-var response = JSON.parse(xhr.response);
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries');
 
-function getJoke() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.addEventListener('load', function(){
-        var response = JSON.parse(xhr.response);
-        paragraph.innerHTML = response.value.joke;
+$('#search').click(searchCountries);
+
+function searchCountries() {
+    var countryName = $('#country-name').val();
+    if(!countryName.length) countryName = 'Poland';
+    $.ajax({
+        url: url + countryName,
+        method: 'GET',
+        success: showCountriesList
     });
-    xhr.send();
+}
+
+function showCountriesList(resp) {
+    countriesList.empty();
+    resp.forEach(function(item){
+        $('<li>').text(item.name).appendTo(countriesList);
+    });
 }
